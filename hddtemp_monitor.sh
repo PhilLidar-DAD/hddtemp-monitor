4795e00e47feb924ea0b62867cc905ed3a8ace98
Kenneth V. Langga <klangga@gmail.com>
2017-05-18 19:01:41 +0800

    Initial code commit

---
+++ hddtemp_monitor.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+TEMP_THRESHOLD=40
+
+# Get list of hdds
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    hdds=($( ls -ahl /dev/disk/by-id/ | egrep "ata|scsi" | grep -v "part" | \
+awk -F "/" '{print $NF}'))
+elif [[ $OSTYPE == "FreeBSD" ]]; then
+    hdds=($( camcontrol devlist | grep ",da" | \
+awk -F "[,)]" '{print $(( NF - 1 ))}' ))
+fi
+# echo "hdds: $hdds"
+
+for hdd in "${hdds[@]}"; do
+
+    smartctl_out=$( smartctl -a /dev/$hdd )
+
+    # ATA
+    temp=$( echo "$smartctl_out" | grep "Temperature_Celsius" | \
+awk '{print $10}' )
+
+    # SCSI
+    if [[ $temp == "" ]]; then
+        # echo "trying scsi..."
+        temp=$( echo "$smartctl_out" | grep "Current Drive Temperature" | \
+awk '{print $(( NF - 1 ))}' )
+    fi
+
+    if [[ $temp != "" ]]; then
+        echo "$hdd: ${temp}C"
+
+        # temp=41
+        if [[ $temp -gt $TEMP_THRESHOLD ]]; then
+
+            title="Emergency shutdown on $( hostname )!"
+
+            hdd_info=$( echo "$smartctl_out" | egrep -i "device model:|product:|serial number:" )
+
+            err_msg="$hdd_info\n\nHDD temp (${temp}C) is over ${TEMP_THRESHOLD}C! Initiating emergency \
+poweroff..."
+
+            # Log to stderr
+            echo -e "\n${title}\n\n${err_msg}\n" >&2
+
+            # Broadcast to wall
+            echo -e "\n${title}\n\n${err_msg}\n" | wall
+
+            # Send email
+            (echo "Subject: $title"; echo; echo -e "$err_msg") | sendmail klangga@gmail.com
+
+            echo "poweroff"
+            break
+        fi
+    fi
+
+done
