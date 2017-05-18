d27089d39522adfd4cd4f31bcbb62ab33caeab13
root <root@proxmox05.srv.dream.upd.edu.ph>
2017-05-18 18:16:20 +0800

    init

--- hddtemp_monitor.sh
+++ hddtemp_monitor.sh
@@ -4,15 +4,39 @@ TEMP_THRESHOLD=40
 
 # Get list of hdds
 if [[ $OSTYPE == "linux-gnu" ]]; then
-    HDDS=$( ls /dev/disk/by-id/ | egrep "ata|scsi" | grep -v "part" )
+    hdds=($( ls -ahl /dev/disk/by-id/ | egrep "ata|scsi" | grep -v "part" | \
+awk -F "/" '{print $NF}'))
 elif [[ $OSTYPE == "FreeBSD" ]]; then
-    HDDS=$( camcontrol devlist | grep ",da" | \
-awk -F "[,)]" '{print $(( NF - 1 ))}' )
+    hdds=($( camcontrol devlist | grep ",da" | \
+awk -F "[,)]" '{print $(( NF - 1 ))}' ))
 fi
-# echo "HDDS: $HDDS"
+# echo "hdds: $hdds"
 
-for HDD in "${HDDS[@]}"; do
-    echo "$HDD"
-    smartctl=$( smartctl /dev/$HDD )
+for hdd in "${hdds[@]}"; do
+
+    smartctl_out=$( smartctl -A /dev/$hdd )
+
+    # ATA
+    temp=$( echo "$smartctl_out" | grep "Temperature_Celsius" | \
+awk '{print $10}' )
+
+    # SCSI
+    if [[ $temp == "" ]]; then
+        # echo "trying scsi..."
+        temp=$( echo "$smartctl_out" | grep "Current Drive Temperature" | \
+awk '{print $(( NF - 1 ))}' )
+    fi
+
+    if [[ $temp != "" ]]; then
+        echo "$hdd: ${temp}C"
+
+        temp=40
+        if [[ $temp -ge $TEMP_THRESHOLD ]]; then
+            echo "HDD temp is over ${TEMP_THRESHOLD}C! Initiating emergency \
+poweroff..." >&2
+            echo "poweroff"
+            break
+        fi
+    fi
 
 done
