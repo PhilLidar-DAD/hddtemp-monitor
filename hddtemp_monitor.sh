66367f4a510a198f2b869d0b1224ae1c8c9c3f86
root <root@ftp-nas.prd.dream.upd.edu.ph>
2017-05-18 17:54:11 +0800

    init

--- hddtemp_monitor.sh
+++ hddtemp_monitor.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+TEMP_THRESHOLD=40
+
 # Get list of hdds
 if [[ $OSTYPE == "linux-gnu" ]]; then
     HDDS=$( ls /dev/disk/by-id/ | egrep "ata|scsi" | grep -v "part" )
 elif [[ $OSTYPE == "FreeBSD" ]]; then
-    echo
+    HDDS=$( camcontrol devlist | grep ",da" | \
+awk -F "[,)]" '{print $(( NF - 1 ))}' )
 fi
 # echo "HDDS: $HDDS"
 
 for HDD in "${HDDS[@]}"; do
     echo "$HDD"
+    smartctl=$( smartctl /dev/$HDD )
+
 done
